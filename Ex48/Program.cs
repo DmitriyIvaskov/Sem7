6be62fb71bea0c82727c0a80af6425c404a5f8a6
DmitriyIvaskov <citrys2008@gmail.com>
2023-06-12 22:31:28 +0700

    Решили 48-9

---
+++ Ex48/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns);
+// rows=3, columns=4 => таблица из 3 строк и 4 столбцов, элемент: число от 0 до 10 включительно
+PrintMatrix(resMatrix); // PrintMatrix(матрица)
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n]; // Таблица из m строк и n столбцов
+    for (int i = 0; i < matrix.GetLength(0); i++) // Цикл по строчкам , i < m
+    {
+        // i,j,m,k
+        for (int j = 0; j < matrix.GetLength(1); j++)// Цикл по стобцам , j < n
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
